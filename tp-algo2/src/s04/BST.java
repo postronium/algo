a14743d2bafd77461fe00657cf3508e71289b8a5
Adrian <adrian.buntschu@edu.hefr.ch>
2018-10-10 18:31:03 +0200

    two first exercices done!

--- tp-algo2/src/s04/BST.java
+++ tp-algo2/src/s04/BST.java
@@ -1,70 +1,134 @@
 package s04;
+
 // =======================
 public class BST<E extends Comparable<E>> {
-  protected BTree<E> tree;
-  protected int   crtSize;
+	protected BTree<E> tree;
+	protected int crtSize;
 
-  public BST() {
-    tree = new BTree<E>();
-    crtSize = 0;
-  }
+	public BST() {
+		tree = new BTree<E>();
+		crtSize = 0;
+	}
 
-  public BST(E [] tab) {  // PRE sorted, no duplicate
-    // TODO - A COMPLETER
-  }
+	public BST(E[] tab) { // PRE sorted, no duplicate
+		// TODO - A COMPLETER
+	}
 
-  /** returns where e is, or where it should be inserted as a leaf */
-  protected  BTreeItr<E> locate(E e) {
-    return null; // TODO - A COMPLETER
-  }
+	/** returns where e is, or where it should be inserted as a leaf */
+	protected BTreeItr<E> locate(E e) {
+		if (this.tree == null) {			//?TODO? ?WTF!!?
+			this.tree = new BTree<E>();		//scchould work without this uggly if statement
+			return new BTreeItr<E>(this.tree);
+		}
+		return this.locateInSubTree(e, new BTreeItr<E>(this.tree));
+	}
+	
+	private BTreeItr<E> locateInSubTree(E e, BTreeItr<E> itr) {
+		if (itr.isBottom()) return itr;						//e not found. e would be inserted here.
+		int diff = e.compareTo(itr.consult());				//negative if e smaller
+		e.compareTo(itr.consult());
+		if (diff == 0) return itr;							//e found at this position
+		if (diff < 0)
+			return this.locateInSubTree(e, itr.left());		//e is somewhere in the left subtree
+		return this.locateInSubTree(e, itr.right());		//e is somewhere in the right subtree
+	}
 
-  public void add(E e) {
-    // TODO - A COMPLETER
-  }
+	public void add(E e) {
+		BTreeItr<E> ePos =  this.locate(e);
+		if (ePos.isBottom())								//if e must be inserted at the bottom
+			ePos.insert(e);										//insert it
+															//else e is already in the tree
+	}
 
-  public void remove(E e) {
-    // TODO - A COMPLETER
-  }
+	public void remove(E e) {
+		BTreeItr<E> ePos =  this.locate(e);
+		if (ePos.isBottom()) 								//e not found, nothing to remove
+			return;
+		
+		if (!ePos.hasLeft() && !ePos.hasRight()) {			//e is a node
+			ePos.up().cut();									//remove e
+			return ;
+		}
+		
+		if (ePos.hasLeft() && ePos.hasRight()) {
+			BTree<E> leftTree = ePos.left().cut();			//save children of e
+			BTree<E> rightTree = ePos.right().cut();
+			ePos.cut();										//remove e
+			BTreeItr<E> itr = new BTreeItr<E>(leftTree);
+			
+			if (itr.isBottom())
+				ePos.cut();
+			
+			while(!itr.right().isBottom())					//go to right most biggest) element
+				itr = itr.right();
+			
+			ePos.paste(itr.cut());
+			ePos.left().paste(leftTree);
+			ePos.right().paste(rightTree);
+		}
+		
+		BTree<E> lower;
+		if (ePos.hasLeft() && !ePos.hasRight()) 			//e hase only left child
+			lower = ePos.left().cut();							//break link below the element to remove
+		else												//only right
+			lower = ePos.right().cut();							//break link below the element to remove
+		ePos.cut();
+		ePos.paste(lower);
+		
+		
+			
+	}
 
-  public boolean contains(E e) {
-    BTreeItr<E> ti = locate(e);
-    return ! ti.isBottom();
-  }
+	public boolean contains(E e) {
+		BTreeItr<E> ti = locate(e);
+		return !ti.isBottom();
+	}
 
-  public int size() {
-    return crtSize;
-  }
+	public int size() {
+		return crtSize;
+	}
 
-  public boolean isEmpty() {
-    return size() == 0;
-  }
+	public boolean isEmpty() {
+		return size() == 0;
+	}
 
-  public E minElt() {
-    return null;  // TODO - A COMPLETER
-  }
+	public E minElt() {
+		BTreeItr<E> itr = new BTreeItr<E>(this.tree);
+		if (!itr.hasLeft() && !itr.hasRight())
+			return itr.consult();
+		while(!itr.isBottom())
+			itr = itr.left();
+		return itr.up().consult();
+	}
 
-  public E maxElt() {
-    return null;  // TODO - A COMPLETER
-  }
+	public E maxElt() {
+		BTreeItr<E> itr = new BTreeItr<E>(this.tree);
+		if (!itr.hasLeft() && !itr.hasRight())
+			return itr.consult();
+		while(!itr.isBottom())
+			itr = itr.right();
+		return itr.up().consult();
+	}
 
-  @Override public String toString() {
-    return ""+tree;
-  }
-  
-  public String toReadableString() {
-    String s = tree.toReadableString();
-    s += "size=="+crtSize+"\n";
-    return s;
-  }
-  
-  // --------------------------------------------------
-  // --- Non-public methods
-  // --------------------------------------------------
-  
-  private BTree<E> optimalBST(E[] sorted, int left, int right) {
-    BTree<E> r = new BTree<E>();
-    BTreeItr<E> ri = r.root();
-    // TODO - A COMPLETER 
-    return r;
-  }
+	@Override
+	public String toString() {
+		return "" + tree;
+	}
+
+	public String toReadableString() {
+		String s = tree.toReadableString();
+		s += "size==" + crtSize + "\n";
+		return s;
+	}
+
+	// --------------------------------------------------
+	// --- Non-public methods
+	// --------------------------------------------------
+
+	private BTree<E> optimalBST(E[] sorted, int left, int right) {
+		BTree<E> r = new BTree<E>();
+		BTreeItr<E> ri = r.root();
+		// TODO - A COMPLETER
+		return r;
+	}
 }
--- tp-algo2/src/s04/BTreeItr.java
+++ tp-algo2/src/s04/BTreeItr.java
@@ -1,214 +1,274 @@
 package s04;
+
 import s04.BTree.BTNode;
+
 public class BTreeItr<E> {
-  private BTree<E> whole;
-  // ------------------------------------------------------------
-  private BTNode<E> up   = null;  
-  private BTNode<E> down = null; 
-  private boolean isLeftArc = false; // not relevant at the root
-  // ------------------------------------------------------------
-  /** Position on root. */
-  public BTreeItr(BTree<E> t) {
-    whole=t;down = whole.root;
-  }
-  
-  // ------------------------------------------------------------
-  // ------ Accessors
-  // ------------------------------------------------------------
-  /** Terminal position: no node above. */
-  public boolean isRoot() {
-    return up==null;
-  }
-  /** Position on a node with a left child. */
-  public boolean hasLeft() { 
-    return down!=null && down.left !=null;
-  }
-  /** Position on a node with a right child. */
-  public boolean hasRight() {
-    return down!=null && down.right!=null;
-  }
-  /** Terminal position: no node below. */
-  public boolean isBottom() {
-    return down==null; 
-  }
-  /** Position on a node with no child. */
-  public boolean isLeafNode() { 
-    return !(isBottom()||hasLeft()||hasRight());
-  }
-  /** Position on a left child. Not relevant if isRoot(). */
-  public boolean isLeftArc() {
-    return isLeftArc;
-  }
-  /** PRE: !isBottom(). Returns the element stored at that position. */
-  public E consult() {
-    assert (!isBottom());
-    assert (isConsistent());
-    return down.elt;
-  }
+	private BTree<E> whole;
+	// ------------------------------------------------------------
+	private BTNode<E> up = null;
+	private BTNode<E> down = null;
+	private boolean isLeftArc = false; // not relevant at the root
+	// ------------------------------------------------------------
 
-  // ------------------------------------------------------------
-  // ------ Movements
-  // ------------------------------------------------------------
+	/** Position on root. */
+	public BTreeItr(BTree<E> t) {
+		whole = t;
+		down = whole.root;
+	}
 
-  /** The tree containing that position */
-  public BTree<E> whole() {
-    return whole;
-  }
-  
-  /** PRE: !isRoot(). Does not move this itr but creates a new one.*/
-  public BTreeItr<E> up() { 
-    assert (!isRoot());
-    BTreeItr<E> r = new BTreeItr<E>(whole);
-    r.down=up; r.up=up.parent; r.isLeftArc = (r.up!=null && r.up.left==r.down);
-    return r;
-  }
-  
-  /** PRE: !isBottom(). Does not move this itr but creates a new one. */
-  public BTreeItr<E> left()      {
-    assert (!isBottom());
-    BTreeItr<E> r = new BTreeItr<E>(whole);
-    r.up=down; r.down=down.left; r.isLeftArc = true;
-    return r;
-  }
-  
-  /** PRE: !isBottom(). Does not move this itr but creates a new one.  */
-  public BTreeItr<E> right()     { 
-    assert (!isBottom());
-    BTreeItr<E> r = new BTreeItr<E>(whole);
-    r.up=down; r.down=down.right; r.isLeftArc = false;
-    return r;
-  }
-  
-  /** Goes down to the left as far as possible, from here. Always returns
-   *  a bottom arc. Does not move this itr but creates a new one.  */
-  public BTreeItr<E> leftMost()  { 
-    BTreeItr<E> r = new BTreeItr<E>(whole);
-    r.up=up; r.down=down; r.isLeftArc = isLeftArc;
-    while (!r.isBottom()) {
-      r.up=r.down; r.down=r.down.left; r.isLeftArc = true;
-    } 
-    return r;
-  }
+	// ------------------------------------------------------------
+	// ------ Accessors
+	// ------------------------------------------------------------
+	/** Terminal position: no node above. */
+	public boolean isRoot() {
+		return up == null;
+	}
 
-  /** Goes down to the right as far as possible, from here. Always returns
-   *  a bottom arc. Does not move this itr but creates a new one.  */
-  public BTreeItr<E> rightMost() {
-    BTreeItr<E> r = new BTreeItr<E>(whole);
-    r.up=up; r.down=down; r.isLeftArc = isLeftArc;
-    while (!r.isBottom()) {
-      r.up=r.down; r.down=r.down.right; r.isLeftArc = false;
-    } 
-    return r;    
-  }
+	/** Position on a node with a left child. */
+	public boolean hasLeft() {
+		return down != null && down.left != null;
+	}
 
-  /** Returns a new itr on the same position. */
-  public BTreeItr<E> alias() {
-    BTreeItr<E> t = new BTreeItr<E>(whole);
-    t.whole = whole; t.up=up; t.down=down; t.isLeftArc=isLeftArc;
-    return t;
-  }
+	/** Position on a node with a right child. */
+	public boolean hasRight() {
+		return down != null && down.right != null;
+	}
 
-  /** Each position belongs to exactly one tree. */
-  public boolean isInside(BTree<E> t) { 
-    return whole==t;
-  }
-  // ------------------------------------------------------------
-  // ------ Modifiers
-  // ------------------------------------------------------------
+	/** Terminal position: no node below. */
+	public boolean isBottom() {
+		return down == null;
+	}
 
-  /** PRE: !isBottom(). Replaces the element stored at that position. */
-  public void  update(E elt) {
-    assert (!isBottom());  assert (isConsistent());
-    down.elt = elt; 
-  }
+	/** Position on a node with no child. */
+	public boolean isLeafNode() {
+		return !(isBottom() || hasLeft() || hasRight());
+	}
 
-  /** Replaces the subtree with a single node containing that element. */
-  public void insert(E elt) {
-    cut();
-    BTNode<E> n = new BTNode<E>(elt, null, null, up);
-    if (isRoot())
-      whole.root=n;
-    else {
-      if (isLeftArc) up.left = n;
-      else           up.right= n;
-    }
-    down = n;
-  }
+	/** Position on a left child. Not relevant if isRoot(). */
+	public boolean isLeftArc() {
+		return isLeftArc;
+	}
 
-  /** Replaces the subtree with that whole tree. 
-   * PRE: !this.isInside(t). POST: t is now empty.*/
-  public void paste(BTree<E> t) {
-    if (isInside(t)) throw new IllegalArgumentException();
-    cut();
-    if (t.isEmpty()) return;
-    BTNode<E> n = t.root; 
-    n.parent=up;
-    if (isRoot())
-      whole.root=n;
-    else {
-      if (isLeftArc) up.left = n;
-      else           up.right= n;
-    }
-    down = n;  
-    t.root = null; 
-  }
+	/** PRE: !isBottom(). Returns the element stored at that position. */
+	public E consult() {
+		assert (!isBottom());
+		assert (isConsistent());
+		return down.elt;
+	}
 
-  /** Removes the subtree and returns it as a new tree. */
-  public BTree<E> cut() {
-    assert (isConsistent());
-    BTree<E> t = new BTree<E>();
-    if (isBottom()) return t;
-    t.root = down; t.root.parent = null;
-    if (isRoot())
-      whole.root=null;
-    else {
-      if (isLeftArc) up.left = null;
-      else           up.right= null;
-    }
-    down = null;
-    return t;
-  }
-  //------------------------------------------------------------
-  //------ "rotation" methods
-  //------------------------------------------------------------
+	// ------------------------------------------------------------
+	// ------ Movements
+	// ------------------------------------------------------------
 
-  /** Moves the neighboring nodes according to a left rotation. 
-   *  PRE: ...                */
-  public void rotateLeft() {
-    // TODO - A COMPLETER...
-  }
+	/** The tree containing that position */
+	public BTree<E> whole() {
+		return whole;
+	}
 
-  /** Moves the neighboring nodes according to a right rotation.
-  *  PRE: ...                 */
-  public void rotateRight() {
-    // TODO - A COMPLETER...
-  }
+	/** PRE: !isRoot(). Does not move this itr but creates a new one. */
+	public BTreeItr<E> up() {
+		assert (!isRoot());
+		BTreeItr<E> r = new BTreeItr<E>(whole);
+		r.down = up;
+		r.up = up.parent;
+		r.isLeftArc = (r.up != null && r.up.left == r.down);
+		return r;
+	}
 
-  @Override public String toString() {
-    return whole.toString();
-  }
-  
-  /** A nice representation of the whole tree */
-  public String toReadableString() {
-    return whole.toReadableString();
-  }
-  //------------------------------------------------------------
-  //------ Non-public methods
-  //------------------------------------------------------------
-  // isConsistent() : tracks tree inconsistency (due to recent 
-  //       PRE-conditions violations for cut/paste/insert) :
-  //       - local bidirectional chaining
-  //       - recursively, upwards, till the root arc
-  //       This slows a lot, but may help detect bugs 
-  // ------------------------------------------------------------
-  private boolean isConsistent() {
-    BTNode<E> u=up, d=down;
-    if (u!=null && d!=((isLeftArc)?(u.left):(u.right))) return false;
-    while (u!=null) {
-      if (d!=null && d.parent!=u) return false;
-      if (d!=u.left && d!=u.right) return false;
-      d=u; u=u.parent;
-    }
-    return (d==whole.root);
-  }
+	/** PRE: !isBottom(). Does not move this itr but creates a new one. */
+	public BTreeItr<E> left() {
+		assert (!isBottom());
+		BTreeItr<E> r = new BTreeItr<E>(whole);
+		r.up = down;
+		r.down = down.left;
+		r.isLeftArc = true;
+		return r;
+	}
+
+	/** PRE: !isBottom(). Does not move this itr but creates a new one. */
+	public BTreeItr<E> right() {
+		assert (!isBottom());
+		BTreeItr<E> r = new BTreeItr<E>(whole);
+		r.up = down;
+		r.down = down.right;
+		r.isLeftArc = false;
+		return r;
+	}
+
+	/**
+	 * Goes down to the left as far as possible, from here. Always returns a bottom
+	 * arc. Does not move this itr but creates a new one.
+	 */
+	public BTreeItr<E> leftMost() {
+		BTreeItr<E> r = new BTreeItr<E>(whole);
+		r.up = up;
+		r.down = down;
+		r.isLeftArc = isLeftArc;
+		while (!r.isBottom()) {
+			r.up = r.down;
+			r.down = r.down.left;
+			r.isLeftArc = true;
+		}
+		return r;
+	}
+
+	/**
+	 * Goes down to the right as far as possible, from here. Always returns a bottom
+	 * arc. Does not move this itr but creates a new one.
+	 */
+	public BTreeItr<E> rightMost() {
+		BTreeItr<E> r = new BTreeItr<E>(whole);
+		r.up = up;
+		r.down = down;
+		r.isLeftArc = isLeftArc;
+		while (!r.isBottom()) {
+			r.up = r.down;
+			r.down = r.down.right;
+			r.isLeftArc = false;
+		}
+		return r;
+	}
+
+	/** Returns a new itr on the same position. */
+	public BTreeItr<E> alias() {
+		BTreeItr<E> t = new BTreeItr<E>(whole);
+		t.whole = whole;
+		t.up = up;
+		t.down = down;
+		t.isLeftArc = isLeftArc;
+		return t;
+	}
+
+	/** Each position belongs to exactly one tree. */
+	public boolean isInside(BTree<E> t) {
+		return whole == t;
+	}
+	// ------------------------------------------------------------
+	// ------ Modifiers
+	// ------------------------------------------------------------
+
+	/** PRE: !isBottom(). Replaces the element stored at that position. */
+	public void update(E elt) {
+		assert (!isBottom());
+		assert (isConsistent());
+		down.elt = elt;
+	}
+
+	/** Replaces the subtree with a single node containing that element. */
+	public void insert(E elt) {
+		cut();
+		BTNode<E> n = new BTNode<E>(elt, null, null, up);
+		if (isRoot())
+			whole.root = n;
+		else {
+			if (isLeftArc)
+				up.left = n;
+			else
+				up.right = n;
+		}
+		down = n;
+	}
+
+	/**
+	 * Replaces the subtree with that whole tree. PRE: !this.isInside(t). POST: t is
+	 * now empty.
+	 */
+	public void paste(BTree<E> t) {
+		if (isInside(t))
+			throw new IllegalArgumentException();
+		cut();
+		if (t.isEmpty())
+			return;
+		BTNode<E> n = t.root;
+		n.parent = up;
+		if (isRoot())
+			whole.root = n;
+		else {
+			if (isLeftArc)
+				up.left = n;
+			else
+				up.right = n;
+		}
+		down = n;
+		t.root = null;
+	}
+
+	/** Removes the subtree and returns it as a new tree. */
+	public BTree<E> cut() {
+		assert (isConsistent());
+		BTree<E> t = new BTree<E>();
+		if (isBottom())
+			return t;
+		t.root = down;
+		t.root.parent = null;
+		if (isRoot())
+			whole.root = null;
+		else {
+			if (isLeftArc)
+				up.left = null;
+			else
+				up.right = null;
+		}
+		down = null;
+		return t;
+	}
+	// ------------------------------------------------------------
+	// ------ "rotation" methods
+	// ------------------------------------------------------------
+
+	/**
+	 * Moves the neighboring nodes according to a left rotation. PRE: ...
+	 */
+	public void rotateLeft() {
+		BTree<E> rightBranche = this.right().cut();
+		BTreeItr<E> rightBrItr = new BTreeItr<E>(rightBranche);
+		this.right().paste(rightBrItr.left().cut());
+		rightBrItr.left().paste(this.cut());
+		this.paste(rightBranche);
+	}
+
+	/**
+	 * Moves the neighboring nodes according to a right rotation. PRE: ...
+	 */
+	public void rotateRight() {
+		BTree<E> leftBranche = this.left().cut();
+		BTreeItr<E> leftBrItr = new BTreeItr<E>(leftBranche);
+		this.left().paste(leftBrItr.right().cut());
+		leftBrItr.right().paste(this.cut());
+		this.paste(leftBranche);
+	}
+
+	@Override
+	public String toString() {
+		return whole.toString();
+	}
+
+	/** A nice representation of the whole tree */
+	public String toReadableString() {
+		return whole.toReadableString();
+	}
+
+	// ------------------------------------------------------------
+	// ------ Non-public methods
+	// ------------------------------------------------------------
+	// isConsistent() : tracks tree inconsistency (due to recent
+	// PRE-conditions violations for cut/paste/insert) :
+	// - local bidirectional chaining
+	// - recursively, upwards, till the root arc
+	// This slows a lot, but may help detect bugs
+	// ------------------------------------------------------------
+	private boolean isConsistent() {
+		BTNode<E> u = up, d = down;
+		if (u != null && d != ((isLeftArc) ? (u.left) : (u.right)))
+			return false;
+		while (u != null) {
+			if (d != null && d.parent != u)
+				return false;
+			if (d != u.left && d != u.right)
+				return false;
+			d = u;
+			u = u.parent;
+		}
+		return (d == whole.root);
+	}
 }
