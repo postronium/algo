0faae7f3cd6624430ea802555658cbb13f99e387
Frederic Bapst <frederic.bapst@hefr.ch>
2018-11-02 09:21:20 +0100

    add s07

---
+++ tp-algo2/src/s07/DisjSets.java
@@ -0,0 +1,40 @@
+package s07;
+
+import java.util.Arrays;
+// ------------------------------------------------------------
+public class DisjSets {
+
+  public DisjSets(int nbOfElements) {
+    // TODO - A COMPLETER
+  }
+
+  public boolean isInSame(int i, int j) {
+    return false;     // TODO - A COMPLETER
+  }
+
+  public void union(int i, int j) {
+    // TODO - A COMPLETER
+  }
+
+  public int nbOfElements() {  // as given in the constructor
+    return 0; // TODO - A COMPLETER
+  }
+  
+  public int minInSame(int i) {
+    return 0; // TODO - A COMPLETER
+  }
+
+  public boolean isUnique() { 
+    return false;    // TODO - A COMPLETER
+  }
+
+  @Override
+  public String toString() {
+    return null;    // TODO - A COMPLETER
+  }
+
+  @Override
+  public boolean equals(Object otherDisjSets) {
+    return false;    // TODO - A COMPLETER
+  }
+}
