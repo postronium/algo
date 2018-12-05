7fee8d7dd231becb59403cb00adaa3e730b7c69e
Adrian <adrian.buntschu@edu.hefr.ch>
2018-12-05 14:33:15 +0100

    TP 09

--- tp-algo2/src/s09/StringSearching.java
+++ tp-algo2/src/s09/StringSearching.java
@@ -1,31 +1,60 @@
 package s09;
 
 import java.util.Random;
+
 // ------------------------------------------------------------ 
 public class StringSearching {
-  // ------------------------------------------------------------ 
-  static /*final*/ int HASHER = 301; // Maybe also try with 7 and 46237
-  static /*final*/ int BASE   = 256; // Please also try with 257
-  // ---------------------
-  static int firstFootprint(String s, int len) {
-    return -1;
-    // TODO - A COMPLETER
-  }
-  // ---------------------
-  // must absolutely be O(1)
-  // coef is (BASE  power  P.LENGTH-1)  mod  HASHER
-  static int nextFootprint(int previousFootprint, char dropChar, char newChar, int coef) {
-    return -1;
-    // TODO - A COMPLETER
-    // h = previousFootprint
-    // h = h - ...               // dropChar        (bien réfléchir !)
-    // h = h * ...               // shift
-    // h = h + ...               // newChar
-  }
-  // ---------------------
-  // Rabin-Karp algorithm
-  public static int indexOf_rk(String t, String p) {
-    return -1;
-    // TODO - A COMPLETER
-  }
+	// ------------------------------------------------------------
+	static /* final */ int HASHER = 301; // Maybe also try with 7 and 46237
+	static /* final */ int BASE = 256; // Please also try with 257
+	// ---------------------
+
+	static int firstFootprint(String s, int len) {
+		int hash = 0;
+		for (int i = 0; i < len; i++) {
+			hash = (hash*BASE + s.charAt(i))%HASHER;
+		}
+		return hash;
+	}
+
+	// ---------------------
+	// must absolutely be O(1)
+	// coef is (BASE power P.LENGTH-1) mod HASHER
+	static int nextFootprint(int previousFootprint, char dropChar, char newChar, int coef) {
+		int hash = previousFootprint;
+		
+		hash = hash - (coef*dropChar)%HASHER;
+		if (hash < 0) hash += HASHER;
+		
+		hash = (hash*BASE + newChar)%HASHER;
+		return hash;
+	}
+
+	// ---------------------
+	// Rabin-Karp algorithm
+	public static int indexOf_rk(String t, String p) {
+		int tHash = firstFootprint(t, p.length());
+		int pHash = firstFootprint(p, p.length());
+		int coef = 1;
+		for (int i = 0; i < p.length()-1; i++)
+			coef = (coef*BASE)%HASHER;
+		
+		if (tHash == pHash && checkEquality(t.substring(0, p.length()), p))
+			return 0;
+		
+		for (int i = 0; i < t.length() - p.length(); i++) {
+			tHash = nextFootprint(tHash, t.charAt(i), t.charAt(i+p.length()), coef);
+			if (tHash == pHash && checkEquality(t.substring(i+1, p.length()+i+1), p))
+				return i+1;
+		}
+		
+		return -1;
+	}
+	
+	private static boolean checkEquality(String t, String p) {
+		for (int i = 0; i < t.length(); i++)
+			if (t.charAt(i) != p.charAt(i))
+				return false;
+		return true;
+	}
 }
