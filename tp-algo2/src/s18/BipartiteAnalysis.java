f1b3d913fdb150feac7a5c74d6b48d138ce3c26f
Adrian <adrian.buntschu@edu.hefr.ch>
2019-03-28 14:57:25 +0100

    S18 + S19

--- tp-algo2/src/s18/BipartiteAnalysis.java
+++ tp-algo2/src/s18/BipartiteAnalysis.java
@@ -4,49 +4,84 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class BipartiteAnalysis {
-  private final UGraph    graph;
-  private boolean         isBipartite;
-  private final boolean[] isVisited;
-  private final boolean[] isWhite;  // meaningful if isBipartite
-  private final int[]     parent;   // the traversal tree structure
-  private List<Integer>   oddCycle; // meaningful if !isBipartite
-  
-  public BipartiteAnalysis(UGraph g) {
-    int n=g.nbOfVertices();
-    graph=g;
-    isVisited=new boolean[n];
-    isWhite=new boolean[n];
-    parent=new int[n];
-    // ... TODO: prepare and drive the depth-first traversal
-  }
-  
-  // Depth-first traversal
-  // PRE: vid is not visited, but has been assigned a color
-  // POST: either the whole subtree has been colored, 
-  //           or an odd cycle has been found via a back edge
-  //              (meaning the graph is not bipartite).
-  // Caution: exit the traversal once you find an "odd-cycle";
-  //          a "back edge" is later met as a "forward" one...
-  private void dft(int vid) {
-    // TODO: A COMPLETER...
-  }
-  
-  // build the oddCycle list with the tree path: <descendantId, ..., ancestorId>
-  private void rememberOddCycle(int ancestorId, int descendantId) {
-    // TODO: A COMPLETER...
-  }
-  
-  public boolean isBipartite() {
-    return isBipartite;
-  }
-  
-  public int colorOf(int vid) {
-    assert isBipartite();
-    return isWhite[vid] ? 0 : 1;
-  }
-  
-  public List<Integer> anOddCycle() {
-    assert !isBipartite();
-    return new ArrayList<Integer>(oddCycle);
-  }
+    private final UGraph graph;
+    private boolean isBipartite;
+    private final boolean[] isVisited;
+    private final boolean[] isWhite;  // meaningful if isBipartite
+    private final int[] parent;   // the traversal tree structure
+    private List<Integer> oddCycle; // meaningful if !isBipartite
+
+    public BipartiteAnalysis(UGraph g) {
+        int n = g.nbOfVertices();
+        graph = g;
+        isVisited = new boolean[n];
+        isWhite = new boolean[n];
+        parent = new int[n];
+
+        isBipartite = true;
+        for (int v = 0; v < g.nbOfVertices() && isBipartite; v++) {
+            dft(v);
+        }
+    }
+
+    // Depth-first traversal
+    // PRE: vid is not visited, but has been assigned a color
+    // POST: either the whole subtree has been colored,
+    //           or an odd cycle has been found via a back edge
+    //              (meaning the graph is not bipartite).
+    // Caution: exit the traversal once you find an "odd-cycle";
+    //          a "back edge" is later met as a "forward" one...
+    private void dft(int vid) {
+        if (isVisited[vid]) return;
+        recDft(vid, false);
+    }
+
+
+    private void recDft(int vid, boolean white) {
+        isVisited[vid] = true;
+        isWhite[vid] = white;
+        for (int n : graph.neighboursOf(vid)) {
+            if (isVisited[n]) {
+                if (white == isWhite[n]) {
+                    rememberOddCycle(n, vid);
+                    isBipartite = false;
+                    break;
+                }
+                continue;
+            }
+            parent[n] = vid;
+            recDft(n, !white);
+            if (!isBipartite) break;
+        }
+    }
+
+    // build the oddCycle list with the tree path: <descendantId, ..., ancestorId>
+    private void rememberOddCycle(int ancestorId, int descendantId) {
+        oddCycle = getSubCycle(ancestorId, descendantId);
+    }
+
+    private ArrayList<Integer> getSubCycle(int ancestorId, int descendantId) {
+        ArrayList oddCycle = new ArrayList<Integer>();
+        oddCycle.add(descendantId);
+        if (ancestorId == descendantId || parent[descendantId] == -1)
+            return oddCycle;
+
+        oddCycle.addAll(getSubCycle(ancestorId, parent[descendantId]));
+
+        return oddCycle;
+    }
+
+    public boolean isBipartite() {
+        return isBipartite;
+    }
+
+    public int colorOf(int vid) {
+        assert isBipartite();
+        return isWhite[vid] ? 0 : 1;
+    }
+
+    public List<Integer> anOddCycle() {
+        assert !isBipartite();
+        return new ArrayList<Integer>(oddCycle);
+    }
 }
--- tp-algo2/src/s18/Kruskal.java
+++ tp-algo2/src/s18/Kruskal.java
@@ -1,33 +1,67 @@
 package s18;
+
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Kruskal {
 
-  /* RETURNS: the total weight of the Minimum Spanning Tree
-   * PRE:     res has the same vertices as g, but no edge
-   * POST:    res is the resulting sub graph
-   */
-  public static int kruskal(WeightedUGraph g, WeightedUGraph res) {
-    // Hint: find the appropriate PriorityQueue constructor, and
-    //       use a lambda expression (together with Integer.compare()).
-    return 0; // TODO - A COMPLETER...
-  }
+    private static class PriorityEdge implements Comparable<PriorityEdge> {
 
-  // ------------------------------------------------------------
-  public static void main(String [] args) {
-    int nVertices = 6; // int nEdges = 12;
-    final int A=0, B=1, C=2, D=3, E=4, F=5;
-    int[] srcs    = {A, A, A, B, B, D, D, D, D, E, F, F };
-    int[] dsts    = {B, C, F, F, C, A, B, C, E, A, D, E };
-    int[] weights = {12,6, 14,1, 7, 9, 3, 2, 4, 11,10,5 };
-    
-    WeightedUGraph g = new WeightedUGraph(nVertices, srcs, dsts, weights);
-    System.out.println("Input Graph: " + g);
+        WeightedUGraph.Edge e;
 
-    WeightedUGraph res = new WeightedUGraph(g.nbOfVertices());
-    int  cost = kruskal(g, res);
-    System.out.println("\nMinimal Spanning Tree of cost: " +cost);
-    for(WeightedUGraph.Edge e:g.allEdges())
-      System.out.println(e.from +" to "+e.to + " of cost " +e.weight);
-  }
+        public PriorityEdge(WeightedUGraph.Edge e) {
+            this.e = e;
+        }
+
+        @Override
+        public int compareTo(PriorityEdge edge) {
+            return e.weight - edge.e.weight;
+        }
+    }
+
+    /* RETURNS: the total weight of the Minimum Spanning Tree
+     * PRE:     res has the same vertices as g, but no edge
+     * POST:    res is the resulting sub graph
+     */
+    public static int kruskal(WeightedUGraph g, WeightedUGraph res) {
+        // Hint: find the appropriate PriorityQueue constructor, and
+        //       use a lambda expression (together with Integer.compare()).
+        Queue<PriorityEdge> edgesByWeight = new PriorityQueue<>();
+        for (WeightedUGraph.Edge e : g.allEdges()) {
+            //edgesByWeight.add((WeightedUGraph.Edge edge) -> e.weight - edge.weight);
+            edgesByWeight.add(new PriorityEdge(e));
+        }
+
+        DisjSets vertexSet = new DisjSets(g.nbOfVertices());
+
+        int totalWeight = 0;
+        while (!edgesByWeight.isEmpty()) {
+            WeightedUGraph.Edge e = edgesByWeight.remove().e;
+            if (!vertexSet.isInSame(e.from, e.to)) {
+                vertexSet.union(e.from, e.to);
+                res.putEdge(e.from, e.to, e.weight);
+                totalWeight += e.weight;
+            }
+        }
+
+        return totalWeight;
+    }
+
+    // ------------------------------------------------------------
+    public static void main(String[] args) {
+        int nVertices = 6; // int nEdges = 12;
+        final int A = 0, B = 1, C = 2, D = 3, E = 4, F = 5;
+        int[] srcs = {A, A, A, B, B, D, D, D, D, E, F, F};
+        int[] dsts = {B, C, F, F, C, A, B, C, E, A, D, E};
+        int[] weights = {12, 6, 14, 1, 7, 9, 3, 2, 4, 11, 10, 5};
+
+        WeightedUGraph g = new WeightedUGraph(nVertices, srcs, dsts, weights);
+        System.out.println("Input Graph: " + g);
+
+        WeightedUGraph res = new WeightedUGraph(g.nbOfVertices());
+        int cost = kruskal(g, res);
+        System.out.println("\nMinimal Spanning Tree of cost: " + cost);
+        for (WeightedUGraph.Edge e : g.allEdges())
+            System.out.println(e.from + " to " + e.to + " of cost " + e.weight);
+    }
 }
